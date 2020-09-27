6eeec94a9f40c16fb9bd2b2b1ab571fc0e6034b7
MaksKrasn <uw5ekr@gmail.com>
2020-09-27 22:49:16 +0300

    Final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,26 @@ import { AngularFireDatabase } from '@angular/fire/database';
 })
 export class AppComponent {
   title = 'firebasetest';
+  items = new Map<string, any>();
 
   constructor(private db: AngularFireDatabase){
     //let root = db.list('/people');
     /*root.push({
-      firstname: 'Fedja',
-      lastname: 'Pupkin',
-      age: 22
+      firstname: 'Timur',
+      lastname: 'Gajdar',
+      age: 45
     })*/
-    let people = db.list('/people')
-    console.log(people);
+    //let people = db.list('/people')
+    //console.log(people);
 
+    db.list('/people').snapshotChanges().subscribe(rez => {
+      this.items.clear();
+      rez.map(element => {
+        this.items.set(element.key, element.payload.exportVal())
+      })
+    })
+
+    console.log(this.items)
   }
 
 }
